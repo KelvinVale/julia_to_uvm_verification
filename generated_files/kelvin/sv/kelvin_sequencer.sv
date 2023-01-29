43bb90c816fa0be1bdde1defd99ec47df2239f9c
Kelvin Vale <kelvin.vale@ee.ufcg.edu.br>
2023-01-29 18:33:05 -0300

    generated file delete

--- generated_files/kelvin/sv/kelvin_sequencer.sv
+++
@@ -1,8 +0,0 @@
-class kelvin_sequencer extends uvm_sequencer#(kelvin_packet);
-    `uvm_component_utils(kelvin_sequencer)
-
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction: new
-
-endclass: kelvin_sequencer
--- generated_files/vip_test/sv/vip_test_sequencer.sv
+++
@@ -1,8 +0,0 @@
-class vip_test_sequencer extends uvm_sequencer#(vip_test_packet);
-    `uvm_component_utils(vip_test_sequencer)
-
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction: new
-
-endclass: vip_test_sequencer
