269eb4bc711c29be3269aa4a8f53feda529228cd
Kelvin Vale <kelvin.vale@ee.ufcg.edu.br>
2023-07-18 17:36:06 -0300

    Stub config updated

--- code_generate_parameters.jl
+++ code_generate_parameters.jl
@@ -2,7 +2,7 @@ reset_generated_files_folder = true
 
 vip_names = ["vip_test", "kelvin"]
 
-stub_if_names = ["vip_test", "someone", "kelvin"]
+stub_if_names = ["vip_test", "kelvin"]
 
 run_vip_gen = true
 run_stub_gen = true
